e31e12bf31f1d126b10800444d6cf7768e77626f
Katrolli <ennio_cano@live.com>
2023-02-20 08:14:16 -0800

    fixiking time

--- models/base_model.py
+++ models/base_model.py
@@ -11,17 +11,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
             self.id = str(uuid.uuid4())
-            self.created_at = (datetime.strptime(
-                datetime.now().isoformat(), '%Y-%m-%dT%H:%M:%S.%f'))
-            self.updated_at = (datetime.strptime(
-                datetime.now().isoformat(), '%Y-%m-%dT%H:%M:%S.%f'))
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.strptime(
-                datetime.now().isoformat(), '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(
-                datetime.now().isoformat(), '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         ''' Printing string repr of instance'''
