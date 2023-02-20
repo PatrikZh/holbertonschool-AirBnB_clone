2f9b842b3e6165e8fc06e18a8121e76d216904e6
Katrolli <ennio_cano@live.com>
2023-02-20 07:42:27 -0800

    pycode

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 
 class BaseModel:
+    '''Base model class'''
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -21,12 +22,15 @@ class BaseModel:
             self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
+        ''' Printing string repr of instance'''
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        ''' update time'''
         self.updated_at = str(datetime.now().isoformat())
 
     def to_dict(self):
+        ''' dict repr of instance'''
         new_d = {}
         new_d = self.__dict__.copy()
         new_d["__class__"] = self.__class__.__name__
