23aaee71354e9108fe2ff347257860e2e308c5ed
Katrolli <ennio_cano@live.com>
2023-02-20 09:10:37 -0800

    fixing id on kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,10 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-            self.id = str(uuid.uuid4())
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
