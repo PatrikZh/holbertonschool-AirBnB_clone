a041167eb5e373b5172279692821e4937a589d77
Katrolli <ennio_cano@live.com>
2023-02-22 04:33:11 -0800

    adding commenting

--- models/place.py
+++ models/place.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''Module of Place'''
 from models.base_model import BaseModel
 
 
--- models/review.py
+++ models/review.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Module of Review"""
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
+class Review(BaseModel):
     ''' city_id will be the same as City.id'''
     city_id = ''
     ''' user_id will be the same as User.id'''
