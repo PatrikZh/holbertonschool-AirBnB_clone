98f8c871037f3ed175f32dcefac2f1a5bc54f355
Katrolli <ennio_cano@live.com>
2023-02-22 04:21:53 -0800

    adding import modules to Filestorage

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
-
+''' City Module'''
 
 class City(BaseModel):
     ''' state_id will be State.id '''
--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
-
+''' Module of State'''
 
 class State(BaseModel):
+    '''State class'''
     name = ''
