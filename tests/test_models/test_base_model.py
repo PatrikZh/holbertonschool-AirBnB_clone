cfe3b02bf2da430d38ce2531e696da2ae7ca292b
Katrolli <ennio_cano@live.com>
2023-02-23 14:09:34 +0100

    testing the tests lol

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Test Modules"""
+import io
+import sys
+import unittest
+import os
+import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_attributes(self):
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertNotEqual(base1.id, base2.id)
+        self.assertNotEqual(base1.created_at, base2.created_at)
+        self.assertNotEqual(base1.updated_at, base2.updated_at)
+
+    def test_attribute_type(self):
+        base1 = BaseModel()
+        self.assertEqual(type(base1.id), str)
+        self.assertEqual(type(base1.created_at), datetime.datetime)
+        self.assertEqual(type(base1.updated_at), datetime.datetime)
