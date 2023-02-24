b958884aecaaaccb8debaff708a71e30f1619c67
FabioH7 <fhysollari4@gmail.com>
2023-02-24 13:33:55 +0100

    adding little changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,12 +16,6 @@ class TestFileStorage(unittest.TestCase):
         obj = FileStorage()
         self.assertEqual(type(obj.all()), dict)
 
-    def test_new(self):
-        obj = FileStorage()
-        base = BaseModel()
-        for value in obj.all().values():
-            if base == obj:
-                self.assertEqual(base, value)
 
     def test_filepath(self):
         obj = FileStorage()
@@ -31,3 +25,11 @@ class TestFileStorage(unittest.TestCase):
         obj = FileStorage()
         self.assertEqual(type(obj._FileStorage__objects), dict)
         self.assertEqual(obj._FileStorage__objects, obj.all())
+
+    def test_new(self):
+        obj = FileStorage()
+        base = BaseModel()
+        obj.new(base)
+        for value in obj.all().values():
+            if base == obj:
+                self.assertEqual(base, value)
