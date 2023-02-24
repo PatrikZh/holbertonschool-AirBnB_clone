95e6535328cf479f7b991c2a1554b935990d5417
FabioH7 <fhysollari4@gmail.com>
2023-02-24 13:48:47 +0100

    adding little changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -20,6 +20,9 @@ class TestFileStorage(unittest.TestCase):
     def test_objects(self):
         obj = FileStorage()
         self.assertEqual(type(obj._FileStorage__objects), dict)
+    
+    def test_all(self):
+        obj = FileStorage()
         self.assertEqual(obj._FileStorage__objects, obj.all())
 
     def test_new(self):
